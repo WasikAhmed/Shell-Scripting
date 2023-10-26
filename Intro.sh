b17a7fa86422a1d845da1befe42be46f4e87de77
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-10-27 00:03:57 +0600

    Add Intro.sh: Demonstrate how to run a script via shell

---
+++ Intro.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# This is a comment
+
+# scripts start with shebang. bash(#) and bang(!)
+# Shebang tells the shell to execute it via bash shell.
+# '#!/bin/zsh' for the Zsh shell
+# '#!/usr/bin/python3' for python3 interpreter
+# can be used to specify various scripting languages (Python, Perl or Ruby)
+
+echo "Hello, World!"
+
+# Modify the file permissions and allow execution of the script using chmod u+x script_name.sh
+# Run script using ./script_name.sh or bash script_name.sh
